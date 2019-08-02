b515184715cae4a47f81eb8b7b46c5cc62931a6c
Stefan <stefanwoj89@ucla.edu>
2019-08-02 09:12:55 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# homework 6
+
+## Video Analysis!
+
+Let's analyse some youtube videos! Unzip the USvideos.csv.zip and ensure the USvideos.csv is in the same directory 
+as videos.py and run the videos.py program. You should get an output of all the data.
+For the assignment, fill in the functions get_most_popular_and_least_popular_channel
+and get_most_liked_and_disliked_channel so that they return the preset dictionaries with the correct
+data. You'll find the most popular and least popular channel by summing up views for each channel, and
+similarly, find the most liked and disliked channel by summing their likes and dislikes. To clarify,
+the most disliked channel has the most dislikes, regardless of how many likes it has, and vice versa. 
+There are duplicates in the data, but for now you can ignore them and consider each entry unique.
+ 
+
+
+
+
---
+++ videos.py
@@ -0,0 +1,67 @@
+import csv
+import pprint
+
+
+def get_video_data():
+    """this function reads from a .csv file and converts the data into a list of dictionaries.
+     each item in the list is a dictionary of a specific videos and their attributes."""
+
+    vid_data = []
+    with open('USvideos.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            if len(row) == 16:
+                vid_dict = {'video_id': row[0],
+                            'trending_date': row[1],
+                            'title': row[2],
+                            'channel_title': row[3],
+                            'category_id': row[4],
+                            'publish_times': row[5],
+                            'tags': row[6],
+                            'views': row[7],
+                            'likes': row[8],
+                            'dislikes': row[9],
+                            'comment_count': row[10],
+                            'thumbnail_link': row[11],
+                            'comments_disabled': row[12],
+                            'ratings_disabled': row[13],
+                            'video_error': row[14],
+                            'description': row[15]
+                            }
+                vid_data.append(vid_dict)
+    return vid_data
+
+
+def print_data(data):
+    for entry in data:
+        pprint.pprint(entry)
+
+
+def get_most_popular_and_least_popular_channel(data):
+    """ fill in the Nones for the dictionary below using the vid data """
+    most_popular_and_least_popular_channel = {'most_popular_channel': None, 'least_popular_channel': None, 'most_pop_num_views': None,
+                                              'least_pop_num_views': None}
+
+    return most_popular_and_least_popular_channel
+
+
+def get_most_liked_and_disliked_channel(data):
+    """ fill in the Nones for the dictionary below using the bar party data """
+
+    most_liked_and_disliked_channel = {'most_liked_channel': None, 'num_likes': None, 'most_disliked_channel': None, 'num_dislikes': None}
+
+    return most_liked_and_disliked_channel
+
+
+if __name__ == '__main__':
+    vid_data = get_video_data()
+
+    # uncomment the line below to see what the data looks like
+    print_data(vid_data)
+
+    popularity_metrics = get_most_popular_and_least_popular_channel(vid_data)
+
+    like_dislike_metrics = get_most_liked_and_disliked_channel(vid_data)
+
+    print('Popularity Metrics: {}'.format(popularity_metrics))
+    print('Like Dislike Metrics: {}'.format(like_dislike_metrics))
